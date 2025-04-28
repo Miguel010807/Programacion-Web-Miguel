776f9c500a4f2179edab87dcb497a715bcd4b741
Miguel010807 <mdiaz@etec.uba.ar>
2025-04-28 16:52:30 -0300

    Hicimos unos ejercicios de if, elif, while, form etec... en  JS(JavaScript) para repasar y luego usarlo con HTML y CSS

---
+++ actividadesJavaScript.js
@@ -0,0 +1,58 @@
+//Nivel 1
+//Actividad IF - 1
+const prueba = 14
+
+if(prueba > 10){
+    console.log(`El numero es mayor a 10`)
+}
+else{
+    console.log(`El numero es menor a 10`)
+}
+
+//Actividad IF - 2
+
+const prueba2 = 7
+
+if(prueba2 > 10){
+    console.log(`El numero es mayor a 10`)
+}
+else{
+    console.log(`El numero es menor a 10`)
+}
+
+//Actividad WHILE - 1
+
+let prueba3 = 5;
+let counter = 0;
+while (counter <= prueba3){
+    counter++;
+    console.log(`el contador ${counter}`);
+}
+
+//Actividad WHILE - 2
+
+let suma = 1;
+let counter1 = 0;
+
+while(counter1 <= 4)
+{
+    suma += counter1;
+    counter1++;
+    console.log(`la suma es: `, suma);
+
+}
+
+//Actividad FOR - 1
+
+for (let i = 1; i <= 4; i++) {
+    console.log("Número: " + i);
+}
+
+//Actividad FOR - 2
+
+let counter2 = 0;
+for (let suma2 = 0; suma2 <= 4; suma2++){
+    counter2+=suma2; 
+    console.log("La suma total es: ", counter2);
+}
+
